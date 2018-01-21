35a7e57322d35e160e2ffb3623d54334ec50899a
Andrei Fyodorov <andrey.fedorov@booking.com>
2018-01-21 03:10:40 +0100

    help and stat commmands

--- app.py
+++ app.py
@@ -16,14 +16,25 @@ bot.setWebhook(url='https://%s/%s' % (settings.WEBHOOK_HOST, settings.TOKEN),
 # print bot.getWebhookInfo()
 
 app = Flask(__name__)
-
-
-REPLIES = ['Go away', 'Get lost', 'Screw you', 'Leave me alone']
-
+stat = dict()
 
 @app.route('/' + settings.TOKEN, methods=['POST'])
 def webhook():
     update = telegram.update.Update.de_json(request.get_json(force=True), bot)
-    bot.sendMessage(chat_id=update.message.chat_id, text=choice(REPLIES))
+    process_message(
+        text=update.message.text,
+        chatkey=update.message.chat_id,
+        send_callback=lambda text: bot.sendMessage(chat_id=update.message.chat_id, text=text)
+    )
 
     return 'OK'
+
+
+def process_message(text, chatkey, send_callback):
+    if 'help' in text:
+        send_callback('words I understand are "help" and "stat"')
+    elif 'stat' in text:
+        send_callback('I sent you %s ahas' % stat.get(chatkey, 0))
+    else:
+        send_callback('aha')
+        stat[chatkey] = stat.get(chatkey, 0) + 1
