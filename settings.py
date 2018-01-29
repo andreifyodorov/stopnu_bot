b9fa150cd66a85bb32734ec82fab936748dcf539
Andrei Fyodorov <andrey.fedorov@booking.com>
2018-01-29 01:54:34 +0100

    Change webhook path

--- settings.py
+++ settings.py
@@ -1,3 +1,3 @@
 TOKEN = '373232718:AAE-WYiLI0HEFA6l3okkl8N1OMhUp_kRwMA'
 CERT = '/usr/local/etc/nginx/cert.pem'
-WEBHOOK_HOST = 'webhooks.bakunin.nl'
+WEBHOOK_HOST = 'webhooks.bakunin.nl/stopnu_bot'
